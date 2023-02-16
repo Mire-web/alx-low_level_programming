51ca2204520ef632e136396dd5b704026d06bf97
mire-web <amahe8664@gmail.com>
2023-02-16 15:29:25 -0800

    quoted quoted

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, 42};
+write(STDERR_FILENO, message, 42);
 return (1);
 }
