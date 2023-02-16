99cd26e4a72e62cd5f123d4e338467b4600c47ff
mire-web <amahe8664@gmail.com>
2023-02-16 15:42:43 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const char* message = a;
 write(STDERR_FILENO, message, 59);
 return (1);
 }
