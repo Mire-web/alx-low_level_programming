371c75fdef2bf92fd3f765d602beaaeffd386b2d
mire-web <amahe8664@gmail.com>
2023-02-16 15:33:21 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  * main - a program to print a quote to standard error
- * return: always 0
+ * Return: always 1;
  */
 int main(void)
 {
 const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, 42);
+write(STDERR_FILENO, message, 59);
 return (1);
 }
