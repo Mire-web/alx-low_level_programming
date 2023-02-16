707fd754c313b0cdf4ed3f2bdae03d70d86c6f32
mire-web <amahe8664@gmail.com>
2023-02-16 15:27:15 -0800

    quoted quoted

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - a program to print a quote to standard error
+ * return: always 0
+ */
+int main(void)
+{
+const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, message, 42};
+return (1);
+}
