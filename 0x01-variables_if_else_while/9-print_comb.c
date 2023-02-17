09278af533b6e33c464935ddbcbe92602709d365
mire-web <amahe8664@gmail.com>
2023-02-17 11:33:29 -0800

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all base 10 numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+putchar((n % 10) + '0');
+putchar(',');
+n += 1;
+}
+putchar('\n');
+return (0);
+}
