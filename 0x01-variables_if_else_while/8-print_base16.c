53334e46678f66f32d2f1c56dc2f2298e207931f
mire-web <amahe8664@gmail.com>
2023-02-17 11:31:04 -0800

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,16 @@
 int main(void)
 {
 int n = 0;
+char c = 'a';
 while (n < 10)
 {
-putchar(n);
+putchar((n % 10) + '0');
 n += 1;
+}
+while (c <= 'f')
+{
+putchar(c);
+c += 1;
 }
 putchar('\n');
 return (0);
