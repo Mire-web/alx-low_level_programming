7bac38439990046202a560bbd7ee5f2ab4fda8dc
mire-web <amahe8664@gmail.com>
2023-02-21 13:21:27 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints all possible combinations of two two-digit numbers
  * Return: Always 0 (Success)
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+/**
+ * main -  a header file for main program
+ * Return: 1 success
+ */
+
+char put_char (char text)
+{
+return text;
+}
