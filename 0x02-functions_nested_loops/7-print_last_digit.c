17be29929e36ffc6a246f723c48b2e1bbc7266f2
mire-web <amahe8664@gmail.com>
2023-02-22 00:24:14 -0800

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - check if alphabet is in lowercase
+ * print_last_digit - print last digit of a number
  * @n: parameter for checks
  * Return: 0 ;
  */
@@ -10,6 +10,6 @@ int print_last_digit(int n)
 {
 int last_digit;
 last_digit = n % 10;
-_putchar(last_digit);
+_putchar(last_digit + '0');
 return (0);
 }
