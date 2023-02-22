edcbbe3e22392eea70f1db6d968b2008c89694c7
mire-web <amahe8664@gmail.com>
2023-02-21 16:58:58 -0800

    main

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
  * Return: 1 if c is lower 0 otherwise;
  */
 
-int print_sign (int n)
+int print_sign(int n)
 {
 if (n > 0)
 {
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - convert to absolute value
+ * @n: parameter for checks
+ * Return: 0 ;
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+n *= -1;
+return n;
+}
+return n;
+}
