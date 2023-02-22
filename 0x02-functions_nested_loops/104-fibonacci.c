07a2c70c69fae34d2a7841c583df8e7a443025b7
mire-web <amahe8664@gmail.com>
2023-02-22 12:56:53 -0800

    gdfdfd

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:int 
+ */
+int main() {
+int i, n = 98, t1 = 1, t2 = 2, nextTerm;
+
+printf("%d, %d, ", t1, t2);
+
+for (i = 3; i <= n; i++) {
+nextTerm = t1 + t2;
+t1 = t2;
+t2 = nextTerm;
+printf("%d", nextTerm);
+if (i < n) {
+printf(", ");
+} else {
+printf("\n");
+}
+}
+return (0);
+}
