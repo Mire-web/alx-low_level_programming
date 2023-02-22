2b69fd7bdb60e3d93910fbe31885c755061c252d
mire-web <amahe8664@gmail.com>
2023-02-22 09:02:33 -0800

    dfdf

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * fib_seq - write out first 50 fib starting with 1 and 2
+ * Return: void
+ */
+void fib_seq(void)
+{
+int n = 1, d = 2, c, tmp,count;
+printf("%d, ", 1);
+printf("%d, ", 2);
+for (count = 0; count <= 50; count++)
+{
+if (count < 50)
+{
+c = d + n;
+printf("%d, ", c);
+tmp = d;
+d = c;
+n = tmp;
+}
+else if (count == 50)
+{
+c = d + n;
+printf("%d", c);
+d = c;
+n = d;
+}
+}
+}
+
+/**
+ * main - entry point
+ *Return:int
+ */
+
+int main(void)
+{
+fib_seq();
+}
