790799e26fd42c277108c5d2c901601d5ae015e1
mire-web <amahe8664@gmail.com>
2023-02-22 02:23:25 -0800

    kill

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,10 +11,7 @@ int n = 0, j, k, l;
 while (n < 3)
 {
 j = 0;
-if (n == 2)
-{
-j = 0;
-while (j < 4)
+while (j < 4 && n == 2 || j < 10 && n != 2)
 {
 k = 0;
 while (k < 6)
@@ -35,30 +32,5 @@ k++;
 j++;
 }
 n++;
-}
-else
-{
-while (j < 10)
-{
-k = 0;
-while (k < 6)
-{
-l = 0;
-while (l < 10)
-{
-_putchar(n + '0');
-_putchar(j + '0');
-_putchar(':');
-_putchar(k + '0');
-_putchar(l + '0');
-_putchar('\n');
-l++;
-}
-k++;
-}
-j++;
-}
-n++;
-}
 }
 }
