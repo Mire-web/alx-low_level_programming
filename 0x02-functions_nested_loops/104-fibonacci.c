abdecd66879d72c465f6b6379bdaf5490efb08ce
mire-web <amahe8664@gmail.com>
2023-02-22 15:03:21 -0800

    dfdfd

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,7 +24,7 @@ for (count = 93; count < 99; count++)
 {
 half1 = n_half1 + n2_half1;
 half2 = n_half2 + n2_half2;
-if (n_half2 + n2_half2 > 99999999999)
+if (n_half2 + n2_half2 > 9999999999)
 {
 half1 += 1;
 half2 %= 10000000000;
