5df2d5854d4e849f525c5b02a00dd9b07c8c5ebe
mire-web <amahe8664@gmail.com>
2023-02-23 09:11:47 -0800

    hit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,21 +8,21 @@
 
 void print_diagonal(int n)
 {
-int i;
-int j;
 if (n <= 0)
 {
 _putchar('\n');
 }
 else
 {
+int i, j;
+
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
 {
 if (j == i)
 _putchar('\\');
-else if (j < i);
+else if (j < i)
 _putchar(' ');
 }
 _putchar('\n');
