5af48157b1b535806e2b1bdbbf1194d073c8c0b5
mire-web <amahe8664@gmail.com>
2023-02-23 09:33:55 -0800

    hit

---
+++ 0x04-more_functions_nested_loops/8-squares.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_squares - draw square in terminal
+ * @size: integer parameter
+ * Return: void
+ */
+
+void print_square(int size)
+{
+if (size <= 0)
+{
+putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+_putchar('#');
+}
+_putchar('\n');
+}
+}
