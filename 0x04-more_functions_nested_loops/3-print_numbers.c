7aaea861b2a43e4aabe5eb39dca9781e0495bde2
mire-web <amahe8664@gmail.com>
2023-02-23 07:52:03 -0800

    fdf

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_numbers - prints numbers from 0-9
  * Return: void
@@ -6,7 +7,7 @@
 void print_numbers(void)
 {
 int i;
-for (i = '0'; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
 _putchar(i + '0');
 }
