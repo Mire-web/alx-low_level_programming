9408115f901aa413259b8e67fe25b1108488cad7
mire-web <amahe8664@gmail.com>
2023-02-27 12:34:28 -0800

    done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - return string reversed
+ * @s: pointer
+ */
+
+void print_rev(char *s)
+{
+int i = 0;
+while (*(s+i) != '\0')
+{
+i++;
+}
+int j = i;
+while (j >= 0)
+{
+_putchar(s[j]);
+j--;
+}
+_putchar('\n');
+}
