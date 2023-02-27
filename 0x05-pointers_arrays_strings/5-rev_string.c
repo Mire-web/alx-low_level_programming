fe41a1eb5b8d7e69db890876fa9eb3f8eb7bf4f9
mire-web <amahe8664@gmail.com>
2023-02-27 13:59:55 -0800

    done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,20 +7,17 @@
 
 void rev_string(char *s)
 {
-int i = 0;
-int j;
-while (*s != '\0')
+char rev = s[0];
+int counter = 0;
+int i;
+
+while (s[counter] != '\0')
+counter++;
+for (i = 0; i < counter; i++)
 {
-i++;
-s++;
+counter--;
+rev = s[i];
+s[i] = s[counter];
+s[counter] = rev;
 }
-s--;
-j = i;
-while (j > 0)
-{
-_putchar(*s);
-j--;
-s--;
-}
-_putchar('\n');
 }
