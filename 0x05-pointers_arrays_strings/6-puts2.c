8d7afccb31bde9ab546a68ceee4c118c5ad0e070
mire-web <amahe8664@gmail.com>
2023-02-27 14:29:02 -0800

    done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,12 +5,25 @@
  * @s: pointer
  */
 
-void puts2(char *s)
+void puts2(char *str)
 {
-while (*s != '\0')
+int longi = 0;
+int t = 0;
+char *y = str;
+int o;
+
+while (*y != '\0')
 {
-_putchar(*s);
-s += 2;
+y++;
+longi++;
+}
+t = longi - 1;
+for (o = 0 ; o <= t ; o++)
+{
+if (o % 2 == 0)
+{
+_putchar(str[o]);
+}
 }
 _putchar('\n');
 }
