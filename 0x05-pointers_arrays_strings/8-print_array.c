3b54ee6b8e46f9bef9910e3a4cc8e006bf2a534f
mire-web <amahe8664@gmail.com>
2023-02-27 15:31:14 -0800

    done

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements in array
+ * @a: pointer
+ * @n: no of elements
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d, ", a[i]);
+}
+putchar('\n');
+}
