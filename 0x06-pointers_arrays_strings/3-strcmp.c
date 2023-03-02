83d5c9098858e7bd8d30d960d1dc963cd16f0dab
mire-web <amahe8664@gmail.com>
2023-03-02 02:38:09 -0800

    Done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 int count = 0;
 while (s1[count] != '\0' && s2[count] != '\0')
 {
-if (s1[count] != s2[count)
+if (s1[count] != s2[count])
 {
 return (s1[count] - s2[count]);
 }
