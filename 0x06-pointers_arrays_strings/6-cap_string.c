d7c11155cca8dd7ec63ec17cd208f5b285d03b86
mire-web <amahe8664@gmail.com>
2023-03-02 04:13:19 -0800

    Done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - captilize every word in string
+ * @a: pointer to string
+ * Return: Char *
+ */
+
+char *cap_string(char *a)
+{
+int count = 1;
+if (a[0] >= 'a' && a[0] <= 'z')
+a[0] = a[0] - 32;
+while (a[count] != '\0')
+{
+if (a[count] < 'A' || a[count] > 'z')
+{
+count++;
+continue;
+}
+else if (a[count] >= 'a' && a[count] <= 'z')
+{
+if (a[count - 1] == ' ' || a[count - 1] == '\t' || a[count - 1] == '\n' || a[count - 1] == ',' || a[count - 1] == '.' || a[count - 1] == '!' || a[count - 1] == ';' || a[count - 1] == '?' || a[count - 1] == '"' || a[count - 1] == '(' || a[count - 1] == ')' || a[count - 1] == '{' || a[count - 1] == '}')
+a[count] -= 32;
+}
+count++;
+}
+return (a);
+}
