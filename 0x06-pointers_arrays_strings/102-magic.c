5532a642620adcb0f464bf35b37d9bc14f22fcb4
mire-web <amahe8664@gmail.com>
2023-03-02 13:35:59 -0800

    done

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,17 +2,17 @@
 
 /**
  * main - access memory in an impossible way
- * Return: 0; 
+ * Return: 0;
  */
 
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+int n;
+int a[5];
+int *p;
 
-  a[2] = 1024;
-  p = &n;
+a[2] = 1024;
+p = &n;
   /*
    * write your line of code here...
    * Remember:
@@ -21,8 +21,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p - 2) = 98;
+*(p - 2) = 98;
   /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
