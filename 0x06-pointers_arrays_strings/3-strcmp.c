838fc766827ffd0d1da91015a11b90b8e066552f
mire-web <amahe8664@gmail.com>
2023-03-02 02:35:57 -0800

    Done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,25 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int total_s1 = 0;
-int total_s2;
-int final_total = 0;
-while (*s1 != '\0')
+int count = 0;
+while (s1[count] != '\0' && s2[count] != '\0')
 {
-total_s1 += *s1;
-s1++;
-}
-total_s2 = 0;
-while (*s2 != '\0')
+if (s1[count] != s2[count)
 {
-total_s2 += *s2;
-s2++;
+return (s1[count] - s2[count]);
 }
-final_total = total_s1 - total_s2;
-if (final_total < 0)
-return (-15);
-else if (final_total == 0)
+count++;
+}
 return (0);
-else
-return (15);
 }
