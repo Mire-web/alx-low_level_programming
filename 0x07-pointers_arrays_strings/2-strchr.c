7581dda9e3551c4b3fbf98d5181770fe9b772483
mire-web <amahe8664@gmail.com>
2023-03-06 05:52:27 -0800

    Done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - find character
+ *
+ * @s: Pointer to string
+ * @c: character to find
+ * Return:char*
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+char *p;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+p = &s[i];
+return (p);
+}
+}
+return (NULL);
+}
