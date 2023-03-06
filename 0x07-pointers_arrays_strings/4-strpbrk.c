68f455c32d38d7d4df5c973bf93bafb80f43257e
mire-web <amahe8664@gmail.com>
2023-03-06 06:57:00 -0800

    Done

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,10 +13,10 @@ char *_strpbrk(char *s, char *accept)
 {
 int i;
 char *p;
-for (i = 0; s[i] >= '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 int j;
-for (j = 0; accept[j] >= '\0'; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 {
