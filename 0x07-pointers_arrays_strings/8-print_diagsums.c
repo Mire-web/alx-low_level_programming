af3519240289c89ffdf081440089e6037b1edfab
mire-web <amahe8664@gmail.com>
2023-03-06 17:05:51 -0800

    done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,11 +13,11 @@ int i, j;
 int sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++)
 {
-sum1 += a[i *size + i];
+sum1 += a[i * size + i];
 }
-for (j =size - 1; j >= 0; j--)
+for (j = size - 1; j >= 0; j--)
 {
-sum2 += a[j *size + (size - 1 - j)];
+sum2 += a[j * size + (size - 1 - j)];
 }
 printf("%d, %d\n", sum1, sum2);
 }
