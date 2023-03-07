dc6db0f8ffcd65b1c612fd6336e60ed1aa811262
mire-web <amahe8664@gmail.com>
2023-03-06 17:04:35 -0800

    done

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - print sum of diagonals
+ * @a:pointer to array
+ * @size: size of array
+ */
+
+void print_diagsums(int *a, int size)
+{
+int i, j;
+int sum1 = 0, sum2 = 0;
+for (i = 0; i < size; i++)
+{
+sum1 += a[i *size + i];
+}
+for (j =size - 1; j >= 0; j--)
+{
+sum2 += a[j *size + (size - 1 - j)];
+}
+printf("%d, %d\n", sum1, sum2);
+}
