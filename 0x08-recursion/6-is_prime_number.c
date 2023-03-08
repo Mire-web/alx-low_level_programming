3c80a28a6f347d262b2a41ca7ef75af4e85a2044
mire-web <amahe8664@gmail.com>
2023-03-08 10:47:27 -0800

    done

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * is_prime - check primality of a number
+ *
+ * @n: int to against
+ * @a: int to check with
+ * Return:int
+ */
+
+int is_prime(int n, int a)
+{
+if (a == 1)
+return (1);
+if (n % a == 0 && a > 0)
+return (0);
+return (is_prime(n, n-1));
+}
+
+/**
+ * is_prime_number- return the result of prime checking
+ *
+ * @n: int to find root
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+if (n <= 1)
+return(0);
+return (is_prime(n, n - 1));
+}
