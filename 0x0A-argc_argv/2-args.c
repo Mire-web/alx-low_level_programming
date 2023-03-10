1e6af559e5ef0019b412914f91757c8f46223d52
mire-web <amahe8664@gmail.com>
2023-03-10 12:18:59 -0800

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print command line args
+ *
+ * @argc: command line argument counter
+ * @argv: Array of strings
+ * Return:int
+ */
+
+int main(int argc, char* argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
