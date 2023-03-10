193e92b7882dbc4482897a0befdc92ef46a9f8ea
mire-web <amahe8664@gmail.com>
2023-03-10 11:10:41 -0800

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print name of program
+ *
+ * @argc: command lline argument counter
+ * @argv: Array of strings
+ * Return:int
+ */
+
+int main(int argc, char *argv[])
+{
+printf("My name is %s\n", argv[0]);
+return(0);
+}
