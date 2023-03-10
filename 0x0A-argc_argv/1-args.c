b92a6e71c7e32eb381edb0fd0fc294590cfe8720
mire-web <amahe8664@gmail.com>
2023-03-10 12:03:05 -0800

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print name of program
+ *
+ * @argc: command lline argument counter
+ * @argv: Array of strings
+ * Return:int
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
