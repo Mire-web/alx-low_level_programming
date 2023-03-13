bb0d1987197b94d76ee411f6a04461b0dbe95059
mire-web <amahe8664@gmail.com>
2023-03-13 03:01:59 -0700

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,7 +17,7 @@ int i;
 int min = 0;
 int num = atoi(argv[1]);
 
-if (argc != 2 || !num)
+if (argc != 2)
 {
 printf("Error\n");
 return (1);
@@ -26,6 +26,7 @@ return (1);
 if (num < 0)
 {
 printf("0\n");
+return (1);
 }
 
 for (i = 0; i < 5; i++)
