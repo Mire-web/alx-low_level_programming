99216ac84e0c015049b092681af07563371d178d
mire-web <amahe8664@gmail.com>
2023-03-14 04:25:38 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,10 @@
 char *create_array(unsigned int size, char c)
 {
 char *ptr;
+unsigned int i;
 ptr = malloc(sizeof(char) * size);
 if (size == 0 || ptr == NULL)
 return (NULL);
-unsigned int i;
 for (i = 0; i < size; i++)
 ptr[i] = c;
 return (ptr);
