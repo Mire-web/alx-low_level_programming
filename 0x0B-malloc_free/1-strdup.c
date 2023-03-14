a747dc156101d3de46254c8ec5ca2b1a8dcb9f0d
mire-web <amahe8664@gmail.com>
2023-03-14 14:57:05 -0700

    done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,10 +13,12 @@ char *_strdup(char *str)
 char *ptr;
 int i;
 int size;
+if (str == NULL)
+return (NULL);
 for (i = 0; str[i] != '\0'; i++)
 size += 1;
 ptr = malloc(sizeof(char) * (size + 1));
-if (str == NULL || ptr == NULL)
+if (ptr == NULL)
 return (NULL);
 for (i = 0; str[i]; i++)
 ptr[i] = str[i];
