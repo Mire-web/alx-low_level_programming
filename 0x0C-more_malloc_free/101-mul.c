10e6f152c7910e786f0ba2e72d5807d6fae20919
mire-web <amahe8664@gmail.com>
2023-03-16 14:00:27 -0700

    done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -42,7 +42,6 @@ void errors(void)
 printf("Error\n");
 exit(98);
 }
-
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
@@ -66,25 +65,20 @@ return (1);
 for (i = 0; i <= len1 + len2; i++)
 result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
-
 {
 digit1 = s1[len1] - '0';
 carry = 0;
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
-
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
-
+carry += result[len1 + len2 + 1] + (digit1 *digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 }
 if (carry > 0)
 result[len1 + len2 + 1] += carry;
 }
-
 for (i = 0; i < len - 1; i++)
-
 {
 if (result[i])
 a = 1;
