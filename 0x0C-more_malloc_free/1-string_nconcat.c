b1b1a3657c7c3f9eeba646555e02a97f6a75e13d
mire-web <amahe8664@gmail.com>
2023-03-16 06:07:59 -0700

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,11 +13,11 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-if (!s1) s1 = "";
-if (!s2) s2 = "";
 int i, j;
 unsigned int k;
 char *ptr;
+if (!s1) s1 = "";
+if (!s2) s2 = "";
 for (i = 0; s1[i] != '\0'; i++)
 j++;
 j += n;
