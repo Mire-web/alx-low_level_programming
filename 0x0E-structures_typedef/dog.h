22f76abc75e04f5118a32b9789af538ad209e2e5
mire-web <amahe8664@gmail.com>
2023-03-20 16:08:12 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog\
-{\
-char *name;\
-float age;\
-char *owner;\
-};\
 
+/**
+ * struct dog - information about a dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of dog Owner
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
 #endif
