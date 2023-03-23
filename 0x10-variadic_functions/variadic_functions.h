f0528a74797487643cdbb10acc6b63946e8a2478
mire-web <amahe8664@gmail.com>
2023-03-23 15:02:14 -0700

    done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,17 @@
 #ifndef VAR_H
 #define VAR_H
+/**
+ * format_t - format parse
+ *
+ */
 
+typedef struct parse_format
+{
+	char c;
+	int i;
+	float f;
+	char *s;
+} format_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
