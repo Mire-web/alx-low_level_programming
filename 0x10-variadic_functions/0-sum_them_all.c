b3f663e1cee7e332f982c53ac7fc70caadcfc352
mire-web <amahe8664@gmail.com>
2023-03-23 09:52:36 -0700

    done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,13 +14,17 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum = 0;
+
 	va_list argparam;
+
 	va_start(argparam, n);
-	
+
 	if (n == 0)
 	return (0);
+
 	for (i = 0; i < n; i++)
 	sum += va_arg(argparam, int);
+
 	va_end(argparam);
 	return (sum);
 }
