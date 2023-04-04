e9be9416dffc2792391c71800718b8429f75aab0
mire-web <amahe8664@gmail.com>
2023-04-04 13:04:21 -0700

    done

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Sum Int of a linkedlist
+ *
+ * @head: pointer to head
+ * Return: int
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr;
+	int sum = 0;
+	if (head == NULL)
+	{
+		return(0);
+	}
+	ptr = head;
+	while (ptr != NULL)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+	return (sum);
+}
