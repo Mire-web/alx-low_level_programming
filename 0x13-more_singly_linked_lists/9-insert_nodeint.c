995babf4f9c55b4ab81b41078ac38f9273acf208
mire-web <amahe8664@gmail.com>
2023-04-04 16:48:57 -0700

    did

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,35 +9,37 @@
  * Return: listint_t*
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-	listint_t *ptr;
-	listint_t *temp = malloc(sizeof(listint_t));
-	unsigned int i = 0;
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n){
+listint_t *ptr;
+listint_t *temp = *head;
+unsigned int i = 0;
 
-	if (!head || !temp)
-		return (NULL);
-	ptr = *head;
-	temp->n = n;
-	temp->next = NULL;
-	if (idx == 0)
-	{
-		temp->next = *head;
-		*head = temp;
-		return (temp);
-	}
-	while (i < (idx - 1))
-	{
-		ptr = ptr->next;
-		i++;
-	}
-	if (i == (idx - 1) && ptr != NULL)
-	{
-		temp->next = ptr->next;
-		ptr->next = temp;
-		return (temp);
-	}
-	else
-		ptr = ptr->next;
-	return (NULL);
+ptr = malloc(sizeof(listint_t));
+if (!ptr || !head)
+return (NULL);
+
+ptr->n = n;
+ptr->next = NULL;
+
+if (idx == 0)
+{
+ptr->next = *head;
+*head = ptr;
+return (ptr);
+}
+
+while (temp && i < idx)
+{
+if (i == idx - 1)
+{
+ptr->next = temp->next;
+temp->next = ptr;
+return (ptr);
+}
+else
+temp = temp->next;
+i++;
+}
+
+return (NULL);
 }
