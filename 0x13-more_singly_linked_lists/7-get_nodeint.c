ab9905c694a14fd706c55622cd59e01d08ae64d5
mire-web <amahe8664@gmail.com>
2023-04-04 13:02:01 -0700

    done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,12 +17,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	return (NULL);
 	ptr = head;
 	i = 0;
-	while (i < index)
+	while (ptr && i < index)
 	{
 		ptr = ptr->next;
 		i++;
 	}
-	if (i == index && ptr != NULL)
+	if (ptr != NULL)
 	return (ptr);
 	else
 {
