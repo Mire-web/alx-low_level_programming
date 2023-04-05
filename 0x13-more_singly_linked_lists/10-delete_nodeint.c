bc49424147dd5a18237266736113a1c4dbf18fa1
mire-web <amahe8664@gmail.com>
2023-04-04 17:11:36 -0700

    mrcheww

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 listint_t *ptr_current = *head;
-listint_t *ptr_previous = head;
+listint_t *ptr_previous = *head;
 unsigned int i = 0;
 
 if (*head == NULL)
