73357f477db4d57da91ec6fa18c2a1d4f37a19aa
Mire <amahe8664@gmail.com>
2023-04-08 20:24:39 +0100

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * get_bit - Get the bit object
+ *
+ * @n: integer to check for bits
+ * @index: index to check for bits
+ * Return: int
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned long int mask = 1 << index;
+if (n & mask)
+{
+return (1);
+}
+else if (!(n & mask))
+{
+return (0);
+}
+
+return(-1);
+}
