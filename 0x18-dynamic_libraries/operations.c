031519782f9661799dc95ba25eaf656275ac7677
Mire <amahe8664@gmail.com>
2023-06-19 19:29:30 +0100

    c_in_py

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ *
+ * @a: int a
+ * @b: int b 
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract one int from another
+ *
+ * @a: subtractor
+ * @b: subtractee
+ * Return: int
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two ints
+ *
+ * @a: int b
+ * @b: int a
+ * Return: int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide one int by another
+ *
+ * @a: numerator
+ * @b: denominator
+ * Return: int
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * sub - subtract one int from another
+ *
+ * @a: numerator
+ * @b: denominator
+ * Return: int
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
