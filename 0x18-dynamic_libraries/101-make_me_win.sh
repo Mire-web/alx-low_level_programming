7557ea0b8da70688fdd28863a2e93847dd0bd578
Mire <amahe8664@gmail.com>
2023-06-24 00:53:03 +0100

    megamillion

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Mire-web/alx-low_level_programming/blob/main/0x18-dynamic_libraries/megamillion.so
+wget -P /tmp https://github.com/Mire-web/alx-low_level_programming/raw/main/0x18-dynamic_libraries/megamillion.so
 export LD_PRELOAD=/tmp/megamillion.so 
