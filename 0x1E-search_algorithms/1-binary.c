22c99e78cfe293061b3452e54e56478b5209f251
Mire <amahe8664@gmail.com>
2024-04-13 20:14:37 +0100

    my-binaryu

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -35,7 +35,7 @@ int binary_search(int *array, size_t size, int value)
 		else if (array[middle] > value)
 			max = middle - 1;
 		else
-			return (array[middle]);
+			return ((int)middle);
 	}
 	return (index);
 }
